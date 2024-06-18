ef6fb56cf7aa6d544b3e2500db0edf24d9b2bb08
Sami Sieranoja <sami.sieranoja@gmail.com>
2024-06-18 12:24:41 +0300

    feat: Add bash integration commands

--- exe/stridx.rb
+++ exe/stridx.rb
@@ -5,6 +5,11 @@ $:.unshift File.dirname(__FILE__) + "/.."
 if ARGV[0] == "tty"
   require "stridx-tty.rb"
   StrIdxTTY.run
+elsif ARGV[0] == "bash"
+  puts %q/
+  bind -m emacs-standard '"\er": redraw-current-line';
+  bind -m emacs-standard '"\C-t": " \C-b\C-k \C-u`stridx.rb tty`\e\C-e\er\C-a\C-y\C-h\C-e\e \C-y\ey\C-x\C-x\C-f"'
+/
 else
   require "daemons"
   Daemons.run(File.dirname(__FILE__) + "/../runserver.rb")
